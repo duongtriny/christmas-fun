267616fdb2a2154cc4a0c8c34e0835a100f663f6
duongtriny <duong.triny@gmail.com>
2023-12-13 21:41:30 +0700

    Completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function initiate() {
   let stars = [];
   let starsR = [];
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 150; i++) {
     let str = `
       <span class="my-star-left" style="--i:${i};">
         <i class="iconoir-star-solid"></i>
@@ -21,36 +21,6 @@ function initiate() {
   html = starsR.join(" ");
   tree = document.getElementsByClassName("r-tree");
   tree[0].innerHTML = html;
-
-
-
-  function animate() {
-    var text = textElement.innerHTML;
-    text = text.replace(" ", "_")
-    var len = text.length
-    var wrapped = "";
-    for (var i in text) {
-      wrapped += '<span>' + text.charAt(i) + '</span>';
-    }
-    textElement.innerHTML = wrapped
-    var delay = 0
-    for (var i = 0; i < len; i++) {
-      var childQuery = "#demo span:nth-child(" + (i + 1) + ")"
-      document.querySelector(childQuery).style.animationDelay = delay + "s";
-      delay += 0.25
-    }
-  
-    document.querySelectorAll('#demo span').forEach(span => span.classList.add('animated'))
-  }
-  
-  document.getElementById('button').addEventListener('click', function() {
-    animate()
-    if(textInput.value === "") {
-      textElement.innerHTML = ""
-    }
-  })
-  
-  animate()
 }
 
 function genLeft() {
